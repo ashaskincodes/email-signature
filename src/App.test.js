e2a59b7648c9bfa889423972b79bd9106e3d53f8
ashaskincodes <ashaskincodes@gmail.com>
2023-08-21 14:49:28 +0100

    project set up complete

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders h1 title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Email Signature Generator/i);
   expect(linkElement).toBeInTheDocument();
 });
