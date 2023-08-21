72ba78e7a73c19510077363c73939162aa5151e4
ashaskincodes <ashaskincodes@gmail.com>
2023-08-21 15:14:15 +0100

    errors fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders h1 title', () => {
+test("renders h1 title", () => {
   render(<App />);
   const linkElement = screen.getByText(/Email Signature Generator/i);
   expect(linkElement).toBeInTheDocument();
